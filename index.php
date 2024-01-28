7cd1462ab284edab6fb2582255d5e772b39f9f22
mackliu <macklun@gmail.com>
2024-01-28 15:38:32 +0800

    加入bootstrap及jquery等套件

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>快樂旅遊網-首頁</title>
+    <link rel="stylesheet" href="./library/bootstrap/bootstrap.css">
+    <link rel="stylesheet" href="./library/fontawesome/fontawesome.css">
+</head>
+<body>
+    
+
+<script src="./library/jquery/jquery.js"></script>
+<script src="./library/bootstrap/bootstrap.js"></script>
+</body>
+</html>
