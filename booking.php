587ba53ac38dad9823b6eb1f73dc9eaeb8c58173
mackliu <macklun@gmail.com>
2024-01-28 16:48:22 +0800

    引入jquery UI來套用萬年曆

--- booking.php
+++ booking.php
@@ -7,13 +7,20 @@
     <title>快樂旅遊網-訪客留言</title>
     <link rel="stylesheet" href="./library/bootstrap/bootstrap.css">
     <link rel="stylesheet" href="./library/fontawesome/fontawesome.css">
+    <link rel="stylesheet" href="./library/jquery-ui-1.13.2/jquery-ui.css">
 </head>
 <body>
 <?php
 $pt=pathinfo(__FILE__);
 include "header.php";
 ?>
+
+<div id="datepicker"></div>
 <script src="./library/jquery/jquery.js"></script>
+<script src="./library/jquery-ui-1.13.2/jquery-ui.js"></script>
 <script src="./library/bootstrap/bootstrap.js"></script>
 </body>
 </html>
+<script>
+$("#datepicker").datepicker();    
+</script>
