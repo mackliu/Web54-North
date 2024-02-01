53d2a4d4511e680b1ca53e7d44721f662097e810
mackliu <macklun@gmail.com>
2024-02-01 17:12:16 +0800

    增加日期選擇判斷邏輯

--- booking.php
+++ booking.php
@@ -201,32 +201,81 @@ function setEvents(){
         $("#start").val(str);
         $("#end").val(str);
         $("#days").val(1);
+        console.log(date)
+        /**
+         * 每一次點擊日期時的判斷
+         * 1. 開始和結束日期都還沒選擇
+         *    a. 此時的點擊日期就是開始日期
+         * 2. 開始日期已經選擇，結束日期還沒選擇
+         *    a. 此時的點擊日期比開始日期還早，則將開始日期設為點擊日期，原本的開始日期變成結束日期
+         *    b. 此時的點擊日期比開始日期還晚，則將結束日期設為點擊日期
+         *    c. 此時的點擊日期和開始日期相同，特殊狀況 1
+         * 3. 開始日期和結束日期都選擇了
+         *    a. 此時的點擊日期如果比開始日期還早，則將開始日期設為點擊日期
+         *    b. 此時的點擊日期如果比結束日期還晚，則將結束日期設為點擊日期
+         *    c. 此時的點擊日期如果在開始日期和結束日期之間，則將結束日期設為點擊日期
+         *    d. 此時的點擊日期和開始日期相同，特殊狀況 2
+         *    e. 此時的點擊日期和結束日期相同，特殊狀況 2
+         * 4. 不可能有先選擇結束日期，再選擇開始日期的情況
+         * 特殊狀況：
+         * 1. 選擇開始日期後，結束日期點在開始日期同一個天的處理
+         * 2. 開始日期和結束日期都選擇後，再次點擊開始或結束日期的處理
+         */
 
-        //判斷選擇的日期是開始日期還是結束日期
-        if(!selectedDateStart || (selectedDateEnd && selectedDateStart)){ //如果開始日期還沒選擇，或者開始日期和結束日期都選擇了
+            //如果開始和結束日期都還沒選擇
+        if(!selectedDateStart  && !selectedDateEnd){ 
             selectedDateStart = date;
             selectedDateEnd = null;
             $(this).addClass("start-date");
         
         }else if(selectedDateStart && !selectedDateEnd){ //如果開始日期已經選擇，結束日期還沒選擇
             
-            if(date >= selectedDateStart){
+            if(date > selectedDateStart){
                 //如果選擇的日期比開始日期還晚，則將結束日期設為選擇的日期
                 selectedDateEnd = date;
                 $(this).addClass("end-date");
         
+            }else if(date.getTime() == selectedDateStart.getTime()){
+                //如果選擇的日期和開始日期相同，則取消開始日期的選擇
+                console.log('選擇的日期和開始日期相同')
+                selectedDateStart = null;
+                $(this).removeClass("start-date");
             }else{
                 //如果選擇的日期比開始日期還早，則將開始日期設為選擇的日期
-                alert("結束日期不能早於開始日期")
-                /* selectedDateEnd = selectedDateStart;
+                //alert("結束日期不能早於開始日期")
+                selectedDateEnd = selectedDateStart;
                 selectedDateStart = date;
-                $(this).addClass("start-date");  */       
+                $('.start-date').addClass("end-date");
+                $('.start-date').removeClass("start-date");
+                $(this).addClass("start-date");        
             }
-            highlightRange(selectedDateStart,selectedDateEnd);
+
+        }else{
+            //如果開始日期和結束日期都選擇了
+            console.log('開始日期和結束日期都選擇了',date.getTime(),selectedDateEnd.getTime())
+            if(date < selectedDateStart){
+                //如果選擇的日期比開始日期還早，則將開始日期設為選擇的日期
+                selectedDateStart = date;
+                $('.start-date').addClass("end-date");
+                $(".start-date").removeClass("start-date");
+                $(this).addClass("start-date");
+
+            }else if(date > selectedDateStart){
+                //如果選擇的日期比開始日期還晚，則將結束日期設為選擇的日期
+                selectedDateEnd = date;
+                $('.end-date').removeClass("end-date");
+                $(this).addClass("end-date");
+                
+            }else if(date.getTime() == selectedDateEnd.getTime()){
+                //如果選擇的日期和結束日期相同，則取消結束日期的選擇
+                console.log('選擇的日期和結束日期相同')
+                selectedDateEnd = null;
+                $(this).removeClass("end-date");
+
+            }
         }
-
+        highlightRange(selectedDateStart,selectedDateEnd);
     })
-
 }
 
 function highlightRange(){
