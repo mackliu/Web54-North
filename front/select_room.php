09a1b2f524d9532429b15a92c024840d1fa0fbb5
mackliu <macklun@gmail.comm>
2024-02-07 22:39:37 +0800

    選擇房號視窗事件處理

--- front/select_room.php
+++ front/select_room.php
@@ -14,15 +14,18 @@
                     </div>
                     <div class="p-3 w-100 d-flex flex-wrap">
                         <?php
-                        $bookedRooms=$_POST['bookedRooms']['unirooms'];
+                        $bookedRooms=$_POST['bookedRooms']['unirooms']??[];
                         for($i=1;$i<=8;$i++){
                             echo "<div class='col-3 p2 my-2' data-num='$i'>";
                             if(in_array($i,$bookedRooms)){
-                                echo "  <div class='text-center rounded border py-3 bg-secondary text-light'>";
+                                echo "  <div class='text-center rounded border py-3 bg-secondary text-light' style='cursor:not-allowed'>";
+                                echo    "Room" . sprintf("%02d",$i);
+                                echo "(己訂)";
                             }else{
-                                echo "  <div class='can-booking text-center rounded border py-3'>";
+                                echo "  <div class='can-booking text-center rounded border py-3' style='cursor:pointer'>";
+                                echo    "Room" . sprintf("%02d",$i);
                             }
-                            echo    "Room" . sprintf("%02d",$i);
+
                             echo "  </div>";
                             echo "</div>";
                         }
@@ -46,5 +49,28 @@ $(".quick").on("click",function(){
         $("#modal").html("");
     });
 });
-
+$(".can-booking").on("click",function(){
+    if($(this).hasClass("bg-info")){
+        $(this).removeClass("bg-info");
+    }else{
+        $(".can-booking").removeClass("bg-info");
+        $(this).addClass("bg-info");
+    }
+});
+$(".cancel").on("click",function(){
+    $(".can-booking").removeClass("bg-info");
+});
+$(".ok").on("click",function(){
+    let roomnum=$(".can-booking.bg-info").parent().data("num");
+    if(roomnum){
+        $("#selectRoom").modal("hide");
+        $("#selectRoom").on("hidden.bs.modal",function(){
+            $("#selectRoom").modal("dispose");
+            $("#modal").html("");
+            $("#roomnum").val(`room${String(roomnum).padStart(2,"0")}`);
+        });
+    }else{
+        alert("請選擇房間");
+    }
+});
 </script>
